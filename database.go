82b0206b18f1b77e206973835b9a3a79d5bfd65f
WayanPande <yande554@gmail.com>
2023-09-21 15:55:41 +0700

    add database

---
+++ database.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/go_htmx?parseTime=true")
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"html/template"
-	"log"
 	"net/http"
-	"time"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type book struct {
-	ID       string `json:"id"`
+	ID       int    `json:"id"`
 	Title    string `json:"title" form:"title"`
 	Author   string `json:"author" form:"author"`
 	Quantity int    `json:"quantity" form:"quantity"`
 }
 
 var books = []book{
-	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
-	{ID: "2", Title: "The Greate Gatsbye", Author: "Scott Fitzgerald", Quantity: 5},
-	{ID: "3", Title: "War and Pieece", Author: "Leo Tolstoy", Quantity: 7},
+	{ID: 1, Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: 2, Title: "The Greate Gatsbye", Author: "Scott Fitzgerald", Quantity: 5},
+	{ID: 3, Title: "War and Pieece", Author: "Leo Tolstoy", Quantity: 7},
 }
 
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
-func getBookById(id string) (*book, error) {
+func getBookById(id int) (*book, error) {
 	for i, b := range books {
 		if b.ID == id {
 			return &books[i], nil
@@ -38,7 +38,11 @@ func getBookById(id string) (*book, error) {
 
 func bookById(c *gin.Context) {
 	id := c.Param("id")
-	book, err := getBookById(id)
+	i, err := strconv.Atoi(id)
+	if err != nil {
+		panic(err)
+	}
+	book, err := getBookById(i)
 
 	if err != nil {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
@@ -61,13 +65,18 @@ func createBooks(c *gin.Context) {
 
 func checkoutBook(c *gin.Context) {
 	id, ok := c.GetQuery("id")
+	i, err := strconv.Atoi(id)
+	if err != nil {
+
+		panic(err)
+	}
 
 	if !ok {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query params"})
 		return
 	}
 
-	book, err := getBookById(id)
+	book, err := getBookById(i)
 
 	if err != nil {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
@@ -83,24 +92,73 @@ func checkoutBook(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, book)
 }
 
-func renderPage(c *gin.Context) {
-	books := map[string][]book{
+func GetData(c context.Context) map[string][]book {
+	db := GetConnection()
+
+	script := "SELECT * FROM book"
+	rows, err := db.QueryContext(c, script)
+	db.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	var books []book
+
+	for rows.Next() {
+		temp_book := book{}
+		rows.Scan(&temp_book.ID, &temp_book.Title, &temp_book.Author, &temp_book.Quantity)
+		books = append(books, temp_book)
+	}
+	rows.Close()
+
+	booksReturn := map[string][]book{
 		"Books": books,
 	}
-	c.HTML(http.StatusOK, "index.html", books)
+
+	return booksReturn
+}
+
+func renderPage(c *gin.Context) {
+
+	c.HTML(http.StatusOK, "index.html", GetData(c))
 }
 
 func addBook(c *gin.Context) {
 	var person book
 
 	if c.ShouldBind(&person) == nil {
-		log.Println(person.Title)
-		log.Println(person.Author)
-		log.Println(person.Quantity)
-		time.Sleep(1 * time.Second)
+		// log.Println(person.Title)
+		// log.Println(person.Author)
+		// log.Println(person.Quantity)
+
+		db := GetConnection()
+		script := "INSERT INTO book(title, author, quantity) VALUES (?, ?, ?)"
+		data, err := db.ExecContext(c, script, person.Title, person.Author, person.Quantity)
+		if err != nil {
+			panic(err)
+		}
+		id, err := data.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		db.Close()
 		tmpl := template.Must(template.ParseFiles("templates/index.html"))
-		tmpl.ExecuteTemplate(c.Writer, "film-list-element", book{Title: person.Title, Author: person.Author, Quantity: person.Quantity})
+		tmpl.ExecuteTemplate(c.Writer, "film-list-element", book{Title: person.Title, Author: person.Author, Quantity: person.Quantity, ID: int(id)})
 	}
+}
+
+func deleteBook(c *gin.Context) {
+	id := c.Param("id")
+
+	db := GetConnection()
+	script := "DELETE FROM book WHERE id=(?)"
+	_, err := db.ExecContext(c, script, id)
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
 }
 
 func main() {
@@ -112,6 +170,9 @@ func main() {
 
 	router.POST("/add-book", addBook)
 
+	router.DELETE("/book/:id", deleteBook)
+
+	// Rest API Stuff
 	router.GET("/books", getBooks)
 	router.POST("/books", createBooks)
 	router.GET("/books/:id", bookById)
